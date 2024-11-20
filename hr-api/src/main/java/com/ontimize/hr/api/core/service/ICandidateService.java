706e180d0749609e97defab25dc45a1930f3c843
Ismael León Cal <ileoncal@gmail.com>
2024-11-20 10:50:01 +0100

    20-11-24

---
+++ hr-api/src/main/java/com/ontimize/hr/api/core/service/ICandidateService.java
@@ -0,0 +1,16 @@
+package com.ontimize.hr.api.core.service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public interface ICandidateService {
+
+    EntityResult candidateQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+    EntityResult candidateInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+    EntityResult candidateUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+    EntityResult candidateDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+}
