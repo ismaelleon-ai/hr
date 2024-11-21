3dba85c9cc3a93228bc4ca63fa58cf3cf5edf3d4
Ismael León Cal <ileoncal@gmail.com>
2024-11-21 10:48:26 +0100

    21-11-24

---
+++ hr-model/src/main/java/com/ontimize/hr/model/core/dao/EducationDao.java
@@ -0,0 +1,15 @@
+package com.ontimize.hr.model.core.dao;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+@Repository("EducationDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/EducationDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class EducationDao extends OntimizeJdbcDaoSupport {
+
+    public static final String ATTR_ID = "ID";
+    public static final String ATTR_DESCRIPTION = "DESCRIPTION";
+}
